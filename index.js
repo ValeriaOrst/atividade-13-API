3c8003ee48fb7f8f8412a5c8d0668d0bcd628acf
ValeriaOrst <165833852+ValeriaOrst@users.noreply.github.com>
2024-11-15 19:46:00 +0000

    info

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;                  // Define a porta
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota GET/ solicitada");
   res.json({
-    nome: "Seu_nome_completo",      // Substitua pelo seu nome
+    nome: "Valéria Ornelas Santos",      // Substitua pelo seu nome
   });
 });
 
